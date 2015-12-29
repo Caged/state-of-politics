68f21328cc6e0dab107c490781464aba1d491126
Justin Palmer <justin@labratrevenge.com>
2015-12-29 13:48:24 -0800

    fix new hampshire district ids so the census and openstates matches

---
+++ sql/normalize-new_hampshire.sql
@@ -0,0 +1,18 @@
+-- Openstates lists new hampshires lower district ids as COUNTY_NAME #.  This doesn't
+-- match the format the US Census Beauro uses, which is a digit.  This converts the
+-- census name to the openstates data district format and then assigns the id to the
+-- district as it is defined by the US census
+with normalized_district_names as (
+  select
+    sldlst,
+    concat_ws(' ',
+      substring(namelsad, '([a-zA-Z]+) County'),
+      substring(namelsad, 'No. ([0-9]+)')) as normalized_name
+  from house_districts
+  where statefp = '33'
+)
+
+update openstates_data
+  set district = sldlst
+  from normalized_district_names
+  where district = normalized_name;
