f75309fb98763eefc2f7e978f098ae1db6448a91
Justin Palmer <justin@labratrevenge.com>
2016-01-03 14:57:35 -0800

    left outer join so we get a record for each district, regardless of match

--- sql/lower-districts.sql
+++ sql/lower-districts.sql
@@ -5,7 +5,7 @@ select leg_id as id,
        district,
        geom
 from house_districts hd
-inner join openstates_data od
+left outer join openstates_data od
   on od.district = hd.sldlst
   and od.state_fips = hd.statefp
   and chamber = 'lower'
