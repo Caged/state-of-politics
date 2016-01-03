000c3682564297e5670bc777cc0b6921a4ba7d98
Justin Palmer <justin@labratrevenge.com>
2016-01-03 10:05:02 -0800

    ensure we get the right state district

--- sql/lower-districts.sql
+++ sql/lower-districts.sql
@@ -1,6 +1,5 @@
 select leg_id as id,
        od.full_name,
-       active,
        party,
        sldlst,
        district,
@@ -8,5 +7,6 @@ select leg_id as id,
 from house_districts hd
 inner join openstates_data od
   on od.district = hd.sldlst
+  and od.state_fips = hd.statefp
   and chamber = 'lower'
   and active;
